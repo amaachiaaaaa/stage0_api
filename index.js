1e03831a62a5d7ae3a0807601fe2858f9453d709
Melisa <melamaachia@gmail.com>
2025-01-31 05:32:20 +0000

    api

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+
+
+
+const app = express();
+
+
+// create middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({
+        email: "melamaachia@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/amaachiaaaaa/stage0_api"
+    });
+});
+
+
+
+
+
+
+
+
+
+// listening for incoming requests
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
